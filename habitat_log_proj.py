cec765a4c32f4463ec79bf1acd07973d45f6a31f
adhr1t <adhrit.srivastav@gmail.com>
2021-08-02 18:42:45 -0500

    updated code

--- habitat_log_proj.py
+++ habitat_log_proj.py
@@ -1,8 +1,28 @@
 import pandas as pd
 import numpy as np
 
-logdf = pd.read_csv('_____')
-gmdf = pd.read_csv('_____')
+logdf = pd.read_csv('Fall_2021_Points_Log.csv')
+gmdf = pd.read_csv('GM_Sign_In_Responses.csv')
+
+# drop Timestamp column
+# gmdf = gmdf.drop(['Timestamp'], axis = 1, inplace = False)
+
+gmdf.value_counts()
+# drop duplicate sign ins on GM sign in spreadsheet
+# gmdf = gmdf.drop_duplicates(subset = 'Last Name')
+
+# parse out the date from the timestamp and drop the time
+gmdf['Timestamp'] = gmdf['Timestamp'].apply(lambda x: str(x).split(' ')[0])
+
+# divide sign-ins by date. Use the first sign-in date as a benchmark and if the sign-in dates are within 3
+# days of that benchmark date then put them into the same dataframe
+type(gmdf['Timestamp'])
+
+aggGMdf = gmdf.groupby(gmdf.Timestamp)
+
+
+while '''next row''' != np.nan():
+    date = gmdf['Timestamp'].split()
 
 # iterate through all the last names and add one to their GM points count
 for i in range(len(gmdf['Last Name']))    :
