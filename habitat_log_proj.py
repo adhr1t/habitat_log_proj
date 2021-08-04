20b4049247b086e6b4d0c24a01b91c584363fd09
adhr1t <adhrit.srivastav@gmail.com>
2021-08-04 07:41:05 -0500

    iterating through GM sign in

--- habitat_log_proj.py
+++ habitat_log_proj.py
@@ -5,9 +5,11 @@ logdf = pd.read_csv('Fall_2021_Points_Log.csv')
 gmdf = pd.read_csv('GM_Sign_In_Responses.csv')
 
 # drop Timestamp column
-# gmdf = gmdf.drop(['Timestamp'], axis = 1, inplace = False)
+gmdf = gmdf.drop(['Timestamp'], axis = 1, inplace = False)
+gmdf = gmdf.rename(columns = {'If you aren\'t on the newsletter and would like to stay up to date, enter your email below:':'newsletter',
+                              'If this row has been visited, put at 1':'visited'})
 
-gmdf.value_counts()
+gmdf.value_counts(gmdf['Last Name'])
 # drop duplicate sign ins on GM sign in spreadsheet
 # gmdf = gmdf.drop_duplicates(subset = 'Last Name')
 
@@ -20,18 +22,27 @@ type(gmdf['Timestamp'])
 
 aggGMdf = gmdf.groupby(gmdf.Timestamp)
 
-
-while '''next row''' != np.nan():
-    date = gmdf['Timestamp'].split()
+gmdf.columns
+logdf.columns
 
 # iterate through all the last names and add one to their GM points count
-for i in range(len(gmdf['Last Name']))    :
-    # check if last and first name at the index is in logdf 
-    if gmdf['Last Name'][i] is in logdf['Last Name'][i] and gmdf['First Name'][i] is in logdf['First Name'][i]:
-        logdf['General Meetings'] += 1
-    
-            
-for i in gmdf['Last Name']:
-    # check if last and first name at the index is in logdf 
-    if gmdf['Last Name'] is in logdf['Last Name']:
-        logdf['General Meetings'] += 1
+for i in range(len(gmdf['Last Name'])):
+    #print(type(gmdf['visited'][i]))
+    if gmdf['visited'][i] != 1:
+        # look for all the empty "divider" lines in between GM days and ignore them so we can get right to the unvisited logs
+        if gmdf['visited'][i-1] == 1 and gmdf['visited'][i+1] == 1:
+            pass
+        else:
+            #print('the i value of ', i, ' is', gmdf['visited'][i])
+            # check if last and first name at the index is in logdf 
+            for j in range(len(logdf['Last Name'])):
+                if (gmdf['Last Name'][i].lower().strip() == logdf['Last Name'][j].lower().strip()) and (gmdf['First Name '][i].lower().strip() == logdf['First Name'][j].lower().strip()):
+                #if (gmdf['Last Name'][i] == logdf['Last Name'][j]) and (gmdf['First Name '][i] == logdf['First Name'][j]):
+                    print(gmdf['Last Name'][i], " is matched by j value of ", j)
+                    logdf['Meetings'] += 1
+
+        
+if (str(gmdf['Last Name'][0]) == str(logdf['Last Name'][0])):
+    print(gmdf['Last Name'][0])
+else:
+    print('bagel')
