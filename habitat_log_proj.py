52d6d1497f78b16d2ac9c59411f16883cf81ae32
adhr1t <adhrit.srivastav@gmail.com>
2021-09-08 02:17:25 -0500

    adds new members

--- habitat_log_proj.py
+++ habitat_log_proj.py
@@ -24,14 +24,40 @@ logdf['Email Address'] = logdf['Email Address'].apply(lambda x: x if pd.isnull(x
 for i in range(len(gmdf['Last Name'])):
     if gmdf['visited'][i] != 1:
         # look for all the empty "divider" lines in between GM days and ignore them so we can get right to the unvisited logs
-        if gmdf['visited'][i-1] == 1 and gmdf['visited'][i+1] == 1:
-            pass
+        #if gmdf['visited'][i-1] == 1 and gmdf['visited'][i+1] == 1: pass
+        # if the row in gmdf is nans, skip the row
+        if pd.isnull(gmdf['First Name'][i]) and pd.isnull(gmdf['Last Name'][i]) and pd.isnull(gmdf['newsletter'][i]): continue
         else:
             # check if last and first name at the index is in logdf 
             for j in range(len(logdf['Last Name'])):
                 if (gmdf['Last Name'][i] == logdf['Last Name'][j]) and (gmdf['First Name'][i] == logdf['First Name'][j]):
-                    #print(gmdf['Last Name'][i], "is matched by j value of ", j)   # this prints the index of matched names
                     logdf['Meetings'][j] += 1
+                    
+                    # if the member has nan/no meeting points, assign the value to 1
+                    if pd.isnull(logdf['Meetings'][j]): logdf['Meetings'][j] = 1
+                    
                     # adds newsletter email into logdf if the cell isn't nan
-                    if not pd.isnull(gmdf['newsletter'][i]):
-                        logdf['Email Address'][j] = gmdf['newsletter'][i]
+                    if not pd.isnull(gmdf['newsletter'][i]): logdf['Email Address'][j] = gmdf['newsletter'][i]
+            
+            # checks logdf for Last Names in gmdf that don't exist yet
+            for k in range(len(logdf['Last Name'])):
+                if gmdf['Last Name'][i] not in logdf['Last Name'][k]: pass
+                else: break
+        
+            # if the for loop reached the end of the Last Names in logdf, we know the gmdf Last Name has no match, so we append to the end of logdf
+            if k == len(logdf['Last Name'])-1:
+                # index for gmdf has to be same as the index of gmdf in the if statement
+                df2 = {'Last Name': gmdf['Last Name'][i], 'First Name': gmdf['First Name'][i], 'Email Address': gmdf['newsletter'][i], 'Meetings': 1}   
+                logdf = logdf.append(df2, ignore_index = True)
+                break
+      
+# sort the dataframe alphabetically
+logdf = logdf.sort_values('First Name')
+                
+                    
+                
+                
+                
+# a test of adding a new entry/member
+df3 = {'Last Name': 'juice', 'First Name': 'carrot', 'newsletter': 'carrotjuice@gmail.com'}
+gmdf = gmdf.append(df3, ignore_index = True)
